c43b8445877ba055c8eec3e02dab14bc1c7436aa
Robin Wellner <gyvox.public@gmail.com>
2011-03-07 00:15:48 +0100

    Converts parse.py from a script to runnable module

--- parse.py
+++ parse.py
@@ -1,22 +1,18 @@
-from errors import *
-
-from nodes import *
-from ifelse import *
-from loop import *
-from func import *
-from trycatch import *
-
 from context import *
 
-filename = 'test'
+def parse(filename):
+	with open(filename) as f:
+		try:
+			filenode = File(filename)
+			filecontext = FileContext(filenode)
+			for i, line in enumerate(f):
+				linecon = LineContext(line.rstrip('\n'), filename, i+1).indent().stringify().decomment().wordify().statementize()
+				filecontext.addline(linecon)
+			return filenode
+		except DejaError as e:
+			print(e)
 
-with open(filename) as f:
-	try:
-		filenode = File(filename)
-		filecontext = FileContext(filenode)
-		for i, line in enumerate(f):
-			linecon = LineContext(line.rstrip('\n'), filename, i+1).indent().stringify().decomment().wordify().statementize()
-			filecontext.addline(linecon)
-		print(filenode)
-	except DejaError as e:
-		print(e)
+if __name__ == '__main__':
+	import sys
+	if len(sys.argv) > 1:
+		print(parse(sys.argv[1]))
