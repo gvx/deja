7ce64bfe210fd39d9d899c7f923679694a56bf96
Robin Wellner <gyvox.public@gmail.com>
2011-09-06 22:01:10 +0200

    Updates flatten.py for the all-new byte code idea

--- flatten.py
+++ flatten.py
@@ -22,6 +22,10 @@ class Branch(FlatNode):
 	def __init__(self, index):
 		self.index = index
 
+class LabdaNode(FlatNode):
+	def __init__(self, index):
+		self.index = index
+
 class FuncDef(FlatNode):
 	def __init__(self, func, name, is_local):
 		self.func = func
@@ -44,7 +48,36 @@ class Marker(object):
 	_I = 0
 	def __init__(self):
 		Marker._I += 1
-		self.i = _I
+		self.i = Marker._I
+
+class SingleInstruction(object):
+	def __init__(self, opcode, ref):
+		self.opcode = opcode
+		self.ref = ref
+
+class FreeLocals(object):
+	def __init__(self):
+		self.used = set()
+	def charify(x):
+		if x > 255:
+			return self.charify(x // 256) + chr(x % 256)
+		return chr(x % 256)
+	def uncharify(x):
+		if len(x) > 1:
+			return self.uncharify(x[1:]) * 256 + ord(x[0])
+		return ord(x)
+	def get_next():
+		i = 0
+		while i in self.used:
+			i += 1
+		self.used.add(i)
+		return '#' + self.charify(i)
+	def free(x):
+		assert x.starts_with('#')
+		assert len(x) > 1
+		self.used.remove(self.uncharify(x[1:]))
+
+free_locals = FreeLocals()
 
 def refine(flattened): #removes all markers and replaces them by indices
 	#first pass: fill dictionary
@@ -75,30 +108,56 @@ def flatten(tree, acc=None):
 			else:
 				acc.append(Code(list(branch.children)))
 		elif isinstance(branch, LabdaStatement):
-			acc.append(FuncDef(refine(flatten(branch)), hasattr(branch, 'name') and branch.name or None, isinstance(branch, LocalFuncStatement)))
-		else isinstance(branch, WhileStatement):
+			m = Marker()
+			acc.append(LabdaNode(m))
+			for argument in branch.arguments:
+				acc.append(SingleInstruction('SET_LOCAL', argument))
+			acc.append(flatten(branch))
+			acc.append(SingleInstruction('RETURN', 0))
+			acc.append(m)
+			if isinstance(branch, LocalFuncStatement):
+				acc.append(SingleInstruction('SET_LOCAL', branch.name))
+			elif isinstance(branch, FuncStatement):
+				acc.append(SingleInstruction('SET', branch.name))
+		elif isinstance(branch, WhileStatement):
 			m1 = Marker()
 			m2 = Marker()
+			acc.append(SingleInstruction('ENTER_CLOSURE', 0))
 			acc.append(m1)
 			flatten(branch.conditionclause, acc)
 			acc.append(Branch(m2))
 			flatten(branch.body, acc)
 			acc.append(GoTo(m1))
 			acc.append(m2)
+			acc.append(SingleInstruction('LEAVE_CLOSURE', 0))
 		elif isinstance(branch, ForStatement):
 			m1 = Marker()
 			m2 = Marker()
 			flatten(branch.forclause, acc)
-			#///SET LOCAL
 			acc.append(m1)
-			acc.append(ForHeader(m2, branch.countername))
+			acc.append(SingleInstruction('DUP', 0))
+			acc.append(Branch(m2))
+			acc.append(SingleInstruction('ENTER_CLOSURE', 0))
+			a = free_locals.get_next()
+			b = free_locals.get_next()
+			acc.append(SingleInstruction('SET_LOCAL', a))
+			acc.append(SingleInstruction('SET_LOCAL', b))
+			acc.append(SingleInstruction('SET_LOCAL', branch.countername))
 			flatten(branch.body, acc)
+			acc.append(SingleInstruction('PUSH_WORD', b))
+			acc.append(SingleInstruction('PUSH_WORD', a))
+			acc.append(SingleInstruction('LEAVE_CLOSURE', 0))
 			acc.append(GoTo(m1))
+			free_locals.free(a)
+			free_locals.free(b)
 			acc.append(m2)
-			pass #???
+			acc.append(SingleInstruction('DROP', 0))
+			acc.append(SingleInstruction('DROP', 0))
+			acc.append(SingleInstruction('DROP', 0))
 		elif isinstance(branch, IfStatement):
 			m_end = Marker()
 			m = Marker()
+			acc.append(SingleInstruction('ENTER_CLOSURE', 0))
 			flatten(branch.ifclause.conditionclause, acc)
 			acc.append(Branch(m))
 			flatten(branch.ifclause, acc)
@@ -113,20 +172,7 @@ def flatten(tree, acc=None):
 				acc.append(m)
 			if branch.elseclause:
 				flatten(branch.elseclause, acc)
-				acc.append(m_end)
-		elif isinstance(branch, CatchStatement):
-			m1 = Marker()
-			m2 = Marker()
-			m_end = Marker()
-			acc.append(PushErrorHandler(m2))
-			acc.append(GoTo(m1))
-			acc.append(m2)
-			flatten(branch.errorhandler, acc)
-			acc.append(GoTo(m_end))
-			acc.append(m1)
-			flatten(branch.body, acc)
-			acc.append(PopErrorHandler())
 			acc.append(m_end)
-			
-			pass #??? #push/pop error handlers?
+			acc.append(SingleInstruction('LEAVE_CLOSURE', 0))
+		#elif isinstance(branch, CatchStatement): # left purposefully unimplemented
 	return acc
