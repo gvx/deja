317c9ccc692ec7811672ad3ab74ee6a8d0ee4b97
Robin Wellner <gyvox.public@gmail.com>
2012-02-10 19:39:31 +0100

    Removes trailing newline on input

--- vm/lib.c
+++ vm/lib.c
@@ -909,6 +909,7 @@ Error input(Header* h, Stack* S, Stack* scope_arr)
 {
 	char line[80];
 	fgets(line, 80, stdin);
+	line[strlen(line) - 1] = '\0'; //removes trailing newline
 	push(S, a_to_value(line));
 	return Nothing;
 }
