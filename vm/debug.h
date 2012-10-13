c06a465abb7c02a158d854cba56b4e1f6ad4367e
Robin Wellner <gyvox.public@gmail.com>
2012-10-13 15:58:26 +0200

    Adds IF_DBG macro

--- vm/debug.h
+++ vm/debug.h
@@ -1,3 +1,3 @@
-#define DBG_PRINT(x) if (vm_debug) { fputs("vm: " x "\n", stderr); }
-#define DBG_PRINTF(x, args...) if (vm_debug) { fprintf(stderr, "vm: " x "\n", args); }
-
+#define IF_DBG(x) if (vm_debug) { x }
+#define DBG_PRINT(x) IF_DBG(fputs("vm: " x "\n", stderr);)
+#define DBG_PRINTF(x, args...) IF_DBG(fprintf(stderr, "vm: " x "\n", args);)
